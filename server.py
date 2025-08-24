8e5ccbed076ada3e385f61de56a7e731dba3375e
TranChiKha030905 <153904433+TranChiKha030905@users.noreply.github.com>
2025-08-25 00:22:47 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_socketio import SocketIO, join_room, leave_room, emit
